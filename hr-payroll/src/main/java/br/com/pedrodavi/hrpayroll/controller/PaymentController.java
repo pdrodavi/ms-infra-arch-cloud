37d0af6fbd53b8b6b0678a150998edd7eae00e65
Pedro Davi <pedrodaviti@gmail.com>
2021-08-01 01:17:48 -0300

    config histryx and timeout

--- hr-payroll/src/main/java/br/com/pedrodavi/hrpayroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/br/com/pedrodavi/hrpayroll/controller/PaymentController.java
@@ -2,6 +2,7 @@ package br.com.pedrodavi.hrpayroll.controller;
 
 import br.com.pedrodavi.hrpayroll.model.entity.Payment;
 import br.com.pedrodavi.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,15 @@ public class PaymentController {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = Payment.builder().name("Pedro").dailyIncome(400.0).days(days).build();
         return ResponseEntity.ok(payment);
     }
 
