9c2fa62a5f2542a06888051eb60df013688a63e2
Pedro Davi <pedrodaviti@gmail.com>
2021-07-31 22:21:38 -0300

    include feingClients and update version spring

---
+++ hr-payroll/src/main/java/br/com/pedrodavi/hrpayroll/service/WorkerFeignClients.java
@@ -0,0 +1,17 @@
+package br.com.pedrodavi.hrpayroll.service;
+
+import br.com.pedrodavi.hrpayroll.model.entity.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerFeignClients {
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Worker> findById(@PathVariable Long id);
+
+}
