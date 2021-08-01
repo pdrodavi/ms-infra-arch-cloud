d13ddf0fd8d3355ad75ed16f19f294d05545015d
Pedro Davi <pedrodaviti@gmail.com>
2021-07-31 21:21:03 -0300

    include project hr-payroll created

---
+++ hr-payroll/src/test/java/br/com/pedrodavi/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedrodavi.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
